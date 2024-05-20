7f2ea34928f98565914c9b526ab5ee97816ad126
Qvverty72 <car.sotoa@duocuc.cl>
2024-05-20 19:09:14 -0400

    footer

--- core/views.py
+++ core/views.py
@@ -6,3 +6,21 @@ def index(request):
     top_prods = Product.objects.all().filter(stock__gt=0).order_by('price')[:3]
     context = { 'top_prods': top_prods }
     return render(request, 'core/index.html', context)
+
+def helpcenter(request):
+    return render(request, 'core/footer/helpcenter.html')
+
+def contactform(request):
+    return render(request, 'core/footer/contactform.html')
+
+def ppolicy(request):
+    return render(request, 'core/footer/ppolicy.html')
+
+def terms(request):
+    return render(request, 'core/footer/terms.html')
+
+def tracking(request):
+    return render(request, 'core/footer/tracking.html')
+
+def whoweare(request):
+    return render(request, 'core/footer/whoweare.html')
