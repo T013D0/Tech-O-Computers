8c2e4161b787f83dbb54afe0aa9de46f98d6c70a
T013D0 <josetoledoo6421@gmail.com>
2024-06-03 09:15:03 -0400

    lol

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from store.models import Product
 
 # Create your views here.
 def index(request):
-    top_prods = Product.objects.all().filter(stock__gt=0).order_by('price')[:3]
+    top_prods = Product.objects.all().filter(stock__gt=0).order_by('price')[:2]
     context = { 'top_prods': top_prods }
     return render(request, 'core/index.html', context)
 
